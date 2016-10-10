0984e35415aca406f74ed19c3afdcb3a95d74a7a
Sascha Weidner <support@sioweb.de>
2016-10-10 10:31:38 +0200

    Update jquery.createForm.js

--- jquery.createForm.js
+++ jquery.createForm.js
@@ -13,9 +13,10 @@
         formClass: 'panel_form',
         formId: 'panel_type',
 
+        generateForm: function(){},
 
         form: [],
-        block: {class:'block'}
+        block: {class:'block',suffix:''}
 	    },
 
   PluginClass = function() {
@@ -61,39 +62,84 @@
       data = $.extend({
         row: 'row',
         class: '',
+        temp: false,
+        return: false,
       },data);
 
+      if(!data.return) {
+        if(selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')] === undefined)
+          selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')] = [];
 
-      if(selfObj.form[selfObj.formClass+'_'+selfObj.block.class] === undefined)
-        selfObj.form[selfObj.formClass+'_'+selfObj.block.class] = [];
+        selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')][selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')].length] = '<div class="'+data.row+(data.class!==''?' '+data.class:'')+'">';
+        selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')][selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')].length] = "\t"+data.field.replace(' >','>');
+        selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')][selfObj.form[(selfObj.block.temp?'temp_':'')+selfObj.formClass+'_'+selfObj.block.class+(selfObj.block.suffix?'_suffix-'+selfObj.block.suffix:'')].length] = '</div>';
+      } else {
+        var block = '';
 
-      selfObj.form[selfObj.formClass+'_'+selfObj.block.class][selfObj.form[selfObj.formClass+'_'+selfObj.block.class].length] = '<div class="'+data.row+(data.class!==''?' '+data.class:'')+'">';
-      selfObj.form[selfObj.formClass+'_'+selfObj.block.class][selfObj.form[selfObj.formClass+'_'+selfObj.block.class].length] = "\t"+data.field.replace(' >','>');
-      selfObj.form[selfObj.formClass+'_'+selfObj.block.class][selfObj.form[selfObj.formClass+'_'+selfObj.block.class].length] = '</div>';
+        block += '<div class="'+data.row+(data.class!==''?' '+data.class:'')+'">';
+        block += "\t"+data.field.replace(' >','>');
+        block += '</div>';
+
+        return block;
+      }
+
+      return false;
     }
 
     this.addBlock = function(data) {
       data = $.extend({
         class: 'block',
+        suffix: '',
+        create: function(){},
       },data);
+
+      data.create(selfObj);
 
       selfObj.block = data;
     };
 
     this.label = function(data) {
-      var field = '';
+      var field = '',
+          tags = false,
+          tags_desc = false;
 
       data = $.extend({
         label: '',
+        id: false,
         required: false,
+        href: false,
+        target: '_blank',
       },data);
 
-      field += '<label for="'+selfObj.formId+'_'+data.name+'">';
+      tags = data.label.match(/{{[^}]+}}/g);
+      tags_desc = data.label.match(/{{[^}]+}}/g);
+
+      field += '<label for="'+selfObj.formId+'_'+(data.id?selfObj.normalize(data.id):selfObj.normalize(data.name))+'"'+(data.description?' class="has_desc"':'')+'>';
+        if(data.href) {
+          if(tags) {
+            data.label = data.label.replace('{{','<a href="'+data.href+'" target="'+data.target+'">');
+            data.label = data.label.replace('}}','</a>');
+          } else {
+            field += '<a href="'+data.href+'" target="'+data.target+'">';
+          }
+        }
         field += data.label+(data.required?'<span class="required">*</span>':'');
+        if(data.href && !tags) field += '</a>';
+        if(data.description) {
+          if(tags_desc) {
+            data.description = data.description.replace('{{','<a href="'+data.href+'" target="'+data.target+'">');
+            data.description = data.description.replace('}}','</a>');
+          }
+          field += '<span class="desc">'+data.description+'</span>';
+        }
       field += '</label>';
 
       return field;
     }
+
+    this.normalize = function(data) {
+      return data.replace(/[^a-zA-Z0-9_\-.:]+/g,'_').replace('__','_');
+    };
 
     this.text = function(data) {
       var field = '';
@@ -106,6 +152,10 @@
         min: false,
         step: false,
         label: '',
+        class: '',
+        id: false,
+        dataAttr: {},
+        select: function(){},
         labelPosition: 'before',
       },data);
 
@@ -114,10 +164,19 @@
       }
 
       field += '<input ';
-        field += 'id="'+selfObj.formId+'_'+data.name+'" ';
+        field += 'id="'+selfObj.formId+'_'+(data.id?selfObj.normalize(data.id):selfObj.normalize(data.name))+'" ';
         field += 'type="'+data.type+'" ';
         field += 'name="'+data.name+'" ';
-        field += 'value="'+data.value+'" ';
+        field += 'tabindex="1" ';
+
+        if(Object.keys(data.dataAttr).length) {
+          for(var a in data.dataAttr) {
+            field += 'data-'+a+'="'+data.dataAttr[a]+'" ';
+          }
+        }
+
+        if(data.value) field += 'value="'+data.value+'" ';
+        if(data.checked) field += 'checked="checked" ';
         if(data.required) field += 'required="required" ';
         if(data.max) field += 'max="'+data.max+'" ';
         if(data.min) field += 'min="'+data.min+'" ';
@@ -128,10 +187,17 @@
         field += selfObj.label(data);
       }
 
-      selfObj.add({
+      return selfObj.add($.extend(data,{
         field: field,
         class: data.type,
-      });
+      }));
+    };
+
+    this.hidden = function(data) {
+      data = $.extend({
+        type: 'hidden',
+      },data);
+      selfObj.text(data);
     };
 
     this.email = function(data) {
@@ -157,7 +223,8 @@
         values: {},
         class: false,
         label: '',
-        labelPosition: 'before'
+        labelPosition: 'before',
+        select: function(){}
       },data);
 
       if(data.label && data.labelPosition === 'before') {
@@ -168,6 +235,7 @@
         field += 'id="'+selfObj.formId+'_'+data.name+'" ';
         field += 'name="'+data.name+'" ';
         field += 'size="'+data.size+'" ';
+        field += 'tabindex="1" ';
         if(data.class) field += 'class="'+data.class+'" ';
         if(data.required) field += 'required="required" ';
       field += '>';
@@ -197,13 +265,65 @@
       selfObj.text(data);
     };
 
+    this.checkboxes = function(data) {
+      var block = '';
+      data = $.extend({
+        type: 'checkbox',
+        value: 1,
+        values: [],
+        return: true,
+        row: 'column',
+        labelPosition: 'after',
+        select: false
+      },data);
+
+
+      if(data.label) {
+        block += selfObj.label(data);
+      }
+
+      for(var v in data.values) {
+        data.values[v].id = data.name+'_'+v;
+        if(data.values[v].value === undefined)
+          data.values[v].value = data.values[v].label;
+        block += selfObj.text($.extend({},true,data,data.values[v]));
+      }
+
+      selfObj.add({
+        field: block,
+        class: 'checkbox_container'
+      });
+    };
+
     this.radio = function(data) {
+      var block = '';
       data = $.extend({
         type: 'radio',
         value: 1,
+        values: [],
+        return: true,
+        row: 'column',
+        select: false,
         labelPosition: 'after',
       },data);
-      selfObj.text(data);
+
+
+      if(data.label) {
+        block += selfObj.label(data);
+      }
+
+      for(var v in data.values) {
+        data.values[v].id = data.name+'_'+v;
+        if(data.values[v].value === undefined)
+          data.values[v].value = data.values[v].label;
+        block += selfObj.text($.extend({},true,data,data.values[v]));
+      }
+
+      selfObj.add({
+        field: block,
+        class: 'radio_container'
+      });
+      
     };
 
     this.textarea = function(data) {
@@ -242,14 +362,71 @@
       });
     };
 
-    this.generate = function() {
-      var form = '';
+    this.addHtml = function(data) {
+      data = $.extend({
+        content: '',
+      },data);
+
+      selfObj.add({
+        field: data.content,
+        class: 'content'
+      });
+    };
+
+    this.generate = function(data) {
+      var returnValue = '';
+
+      data = $.extend({
+        type: 'body',
+        method: 'post',
+        action: false,
+        fieldset: true,
+        removeTemp: true,
+        class: '',
+      },data);
+
       for(var i in selfObj.form) {
-        form += '<div class="'+i+'">'+"\n";
-          form += selfObj.form[i].join("\n");
-        form += "\n"+'</div>'+"\n";
+        var className = i.replace(/_suffix-.*/,'');
+        returnValue += '<div class="'+className+'">'+"\n";
+          returnValue += selfObj.form[i].join("\n");
+        returnValue += "\n"+'</div>'+"\n";
       }
-      return form;
+
+      switch(data.type) {
+        case 'body': 
+        break;
+        case 'form':
+          var pform = '';
+
+          pform += '<form ';
+            if(data.action) pform += 'action="'+data.action+'" ';
+            if(data.method) pform += 'method="'+data.method+'" ';
+            if(data.class) pform += 'class="'+data.class+'" ';
+          pform += '>';
+            if(data.fieldset) pform += '<fieldset>';
+            if(data.fieldset && data.legend) pform += '<legend>'+data.legend+'</legend>';
+            pform += returnValue;
+            if(data.fieldset) pform += '</fieldset>';
+          pform += '</form>';
+
+          returnValue = pform.replace(' >','>');
+        break;
+        default:
+          returnValue = selfObj.generateForm(data,returnValue,selfObj);
+        break;
+      }
+
+      if(data.removeTemp) {
+        var tForm = [];
+        for(var t in selfObj.form) {
+          if(t.split('_')[0] !== 'temp')
+            tForm[t] = selfObj.form[t];
+        }
+
+        selfObj.form = tForm;
+      }
+
+      return returnValue;
     };
   };
 
